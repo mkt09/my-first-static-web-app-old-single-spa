5fbfb7af138aa726e39ab6d76197b89946ad090c
manoj_kumar57 <manoj_kumar57@dell.com>
2022-02-22 10:12:55 +0530

    Update temp 01

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<div>Hello {{value}}</div>`,
+  templateUrl: './app-component.component.html',
+  styleUrls: ['./app-component.component.css']
 })
 export class AppComponent {
   value = 'World';
